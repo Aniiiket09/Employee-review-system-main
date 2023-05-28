955b375421c326078f047719f151f14b5e8bf9b1
aniket_tarapurkar <aniket.v.tarapurkar99@gmail.com>
2023-05-28 11:52:40 +0530

    removed public image folder unwanted

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-console.log('router reloaded');
+console.log("router reloaded");
 
-router.use('/', require('./users'));
-router.use('/review', require('./reviews'));
+router.use("/", require("./users"));
+router.use("/review", require("./reviews"));
 
 module.exports = router;
